92b9c292ef80ed4fef06b430c3ccd9d1f82820f1
dasuma <diego.suarez@rappi.com>
2020-10-16 18:37:25 -0500

    remove logs

--- logs/logs.go
+++ logs/logs.go
@@ -12,9 +12,16 @@ var (
 
 //Start start
 func Start() {
+
 	options := logger.Options{}
 	options.Level = "fatal"
+	options.Hostname = api
 	options.App = api
+	options.IPAddress = "10.0.1.101"
+	options.MacAddress = "C0:FF:EE:C0:FF:EE"
+	options.Tags = "logging,golang"
+	options.Env = "production"
+	options.Tags = "logging,golang"
 	Logger, _ = logger.NewLogger(options, "e038de664579453d90769648448b0e6b")
 
 }
--- rest/consumer.go
+++ rest/consumer.go
@@ -3,11 +3,9 @@ package rest
 import (
 	"bytes"
 	"encoding/json"
-	"fmt"
 	"net/http"
 	"time"
 
-	"github.com/dasuma/blitz-utils-go/logs"
 	"github.com/dasuma/blitz-utils-go/utils"
 )
 
@@ -26,21 +24,16 @@ func HTTPRequest(method string, url string, body interface{}, token *string, set
 	}
 	client := &http.Client{Timeout: 50 * time.Second}
 	resp, err := client.Do(req)
-	buf := new(bytes.Buffer)
-    buf.ReadFrom(resp.Body)
-    newStr := buf.String()
-	message := fmt.Sprintf("end request with url:%s and request %s and response %v", url, body, newStr)
-	logs.Logger.Info(message)
 	return resp, err
 }
 func getReq(method string, url string, body interface{}) (*http.Request, error) {
 	if method == http.MethodGet {
 		return http.NewRequest(method, url, nil)
-	} else {
-		jsonReq, _ := json.Marshal(body)
-		request := bytes.NewReader(jsonReq)
-		return http.NewRequest(method, url, request)
 	}
+	jsonReq, _ := json.Marshal(body)
+	request := bytes.NewReader(jsonReq)
+	return http.NewRequest(method, url, request)
+
 }
 
 //SetHeadersToken set headers braze
