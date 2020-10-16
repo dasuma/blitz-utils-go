1fa2683961982611c63f6976f908181ea5b57d1d
dasuma <diego.suarez@rappi.com>
2020-10-16 12:09:20 -0500

    start logdna

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/blitz-utils-go
 
 go 1.14
 
-require github.com/gin-gonic/gin v1.6.3
+require (
+	github.com/evalphobia/go-logdna v0.1.0 // indirect
+	github.com/gin-gonic/gin v1.6.3
+)
---
+++ logs/logs.go
@@ -0,0 +1,26 @@
+package logs
+
+import (
+	"github.com/blitz-utils-go/utils"
+	"github.com/evalphobia/go-logdna/logdna"
+)
+
+var (
+	api = utils.GetEnv("API", "deev")
+	//Logger
+	Logger *logdna.Client
+)
+
+//Start start
+func Start() {
+	conf := logdna.Config{
+		APIKey:       "e038de664579453d90769648448b0e6b",
+		App:          api,
+		Env:          "production",
+		MinimumLevel: logdna.LogLevelInfo,
+		Sync:         false,
+		Debug:        true,
+	}
+	Logger, _ = logdna.New(conf)
+
+}
