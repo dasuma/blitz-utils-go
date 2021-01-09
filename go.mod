4cff88586fb0930ea1272a10b0219bdddb38c801
diego <diego.suarez@rappi.com>
2021-01-09 10:07:36 -0500

    add logs

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.14
 
 require (
 	github.com/gin-gonic/gin v1.6.3
+	github.com/labstack/gommon v0.3.0
 	github.com/logdna/logdna-go v1.0.1-0.20201002004957-9b3fc06df0c9
 )
--- rest/consumer.go
+++ rest/consumer.go
@@ -3,32 +3,41 @@ package rest
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"time"
 
-	"github.com/dasuma/blitz-utils-go/logs"
 	"github.com/dasuma/blitz-utils-go/utils"
+	"github.com/labstack/gommon/log"
 )
 
 var user101 = utils.GetEnv("user_101", "CUE")
 var password101 = utils.GetEnv("password_101", "123456")
+var errorCode = []int{
+	400, 401, 402, 403, 404, 405, 406, 407, 408, 409, 410, 411, 412, 413, 414, 415, 416, 417, 418, 421, 422, 423, 424, 425, 426,
+	428, 429, 431, 451, 500, 501, 502, 503, 504, 505, 506, 507, 508, 510, 511}
 
 //HTTPRequest request
-func HTTPRequest(method string, url string, body interface{}, token *string, setHeaders func(req *http.Request, userID string) *http.Request) (*http.Response, error) {
-	headers := []Headers{{Key: "Content-Type", Value: "application/json"}}
+func HTTPRequest(method string, url string, body interface{}, result interface{}, userID string, setHeaders func(req *http.Request, userID string) *http.Request) (interface{}, error) {
 	req, _ := getReq(method, url, body)
-	for _, value := range headers {
-		req.Header.Set(value.Key, value.Value)
-	}
-	if token != nil {
-		req = setHeaders(req, *token)
-	}
-	client := &http.Client{Timeout: 50 * time.Second}
+	req = setHeaders(req, userID)
+	client := &http.Client{Timeout: 3 * time.Second}
 	resp, err := client.Do(req)
-	message := fmt.Sprintf("end request with url:%s and request %s and response %v", url, body, resp.StatusCode)
-	logs.Logger.Info(message)
-	return resp, err
+	if ContainsInt(errorCode, resp.StatusCode) {
+		responseData, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Fatalf(err.Error())
+		}
+		message := fmt.Sprintf("error request with url:%s and request %s and code_rescode:%v response %v", url, body, resp.StatusCode, string(responseData))
+		log.Info(message)
+		return nil, errors.New(message)
+	}
+	json.NewDecoder(resp.Body).Decode(&result)
+	message := fmt.Sprintf("end request with url:%s and request %s and code_rescode:%v response %v", url, body, resp.StatusCode, result)
+	log.Info(message)
+	return result, err
 }
 func getReq(method string, url string, body interface{}) (*http.Request, error) {
 	if method == http.MethodGet {
