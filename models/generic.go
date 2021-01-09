3c33a2a50cb3db6073b0c068472d887e45d4c814
diego <diego.suarez@rappi.com>
2021-01-09 10:08:50 -0500

    add generic response

---
+++ models/generic.go
@@ -0,0 +1,14 @@
+package models
+
+//GenericResponse model
+type GenericResponse struct {
+	Response interface{}
+	Error    error
+}
+
+//GenericErrorResponse model
+type GenericErrorResponse struct {
+	Message  string      `json:"message"`
+	Code     int         `json:"code"`
+	Response interface{} `json:"response"`
+}
