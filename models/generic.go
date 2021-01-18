4979e6cddde170ddb3075a5aa33d16febaeb3d70
diego <diego.suarez@rappi.com>
2021-01-17 19:05:07 -0500

    change generic response

--- models/generic.go
+++ models/generic.go
@@ -2,13 +2,8 @@ package models
 
 //GenericResponse model
 type GenericResponse struct {
-	Response interface{}
-	Error    error
-}
-
-//GenericErrorResponse model
-type GenericErrorResponse struct {
-	Message  string      `json:"message"`
-	Code     int         `json:"code"`
-	Response interface{} `json:"result"`
+	Message   string      `json:"message"`
+	Code      int         `json:"code"`
+	Response  interface{} `json:"result"`
+	IsSuccess bool        `json:"is_success"`
 }
