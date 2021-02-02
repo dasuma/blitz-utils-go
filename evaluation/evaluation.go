75f006830fe7233e9bda3c0b1519a3a8f6479bf7
diego <diego.suarez@rappi.com>
2021-02-02 18:34:27 -0500

    add kind

--- evaluation/evaluation.go
+++ evaluation/evaluation.go
@@ -12,3 +12,20 @@ func Start(tax float64, min float64, kind string) float64 {
 	}
 	return res
 }
+
+func ByKind(kind string, value float64, rate float64) float64 {
+	var res float64
+	switch kind {
+	case "percentage":
+		if rate > 1 {
+			res = (value * rate) / 100
+		} else {
+			res = (value * rate)
+		}
+	case "text":
+		res = value
+	case "value":
+		res = value
+	}
+	return res
+}
