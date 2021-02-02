b0e571dbd990c64549faa7af582a473fb34f100c
diego <diego.suarez@rappi.com>
2021-02-02 15:02:31 -0500

    add evaluation

---
+++ evaluation/evaluation.go
@@ -0,0 +1,14 @@
+package evaluation
+
+func evaluateTax(tax float64, min float64, kind string) float64 {
+	res := tax
+	if tax > min {
+		switch kind {
+		case "value":
+			res = min
+		default:
+			res = (tax * min) / 100
+		}
+	}
+	return res
+}
